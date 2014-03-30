0432fa562107a3c1f87003e4f3df04b6552acc70
Benny Neugebauer <bn@bennyn.de>
2014-03-30 15:53:34 +0200

    Pusher works. BLOG

--- wlc-webapp-pusher/src/main/java/com/welovecoding/web/pusher/InsertSQL.java
+++ wlc-webapp-pusher/src/main/java/com/welovecoding/web/pusher/InsertSQL.java
@@ -1,5 +1,8 @@
 package com.welovecoding.web.pusher;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,26 +13,34 @@ import org.apache.ibatis.jdbc.ScriptRunner;
 
 public class InsertSQL {
 
+  private static final Logger LOG = Logger.getLogger(InsertSQL.class.getName());
+
   public static void main(String[] args) throws ClassNotFoundException {
 
     Class.forName("com.mysql.jdbc.Driver");
 
     String url = "jdbc:mysql://localhost:3306/welovecoding";
-
-    String user = System.getProperty("jdbc.user", "welovecoding");
-    String password = System.getProperty("jdbc.password", "123456");
+    String user = System.getProperty("sql.user", "welovecoding");
+    String password = System.getProperty("sql.password", "123456");
+    String script = System.getProperty("sql.script", "test/insert.sql");
 
     Properties info = new Properties();
     info.put("user", user);
     info.put("password", password);
 
-    try {
-      Connection connection = DriverManager.getConnection(url, info);
-      ScriptRunner runner = new ScriptRunner(connection);
-    } catch (SQLException ex) {
-      Logger.getLogger(InsertSQL.class.getName()).log(Level.SEVERE, null, ex);
+    InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(script);
+
+    if (in == null) {
+      LOG.log(Level.WARNING, "Test file not found.");
+    } else {
+      try {
+        Connection connection = DriverManager.getConnection(url, info);
+        ScriptRunner runner = new ScriptRunner(connection);
+        runner.runScript(new BufferedReader(new InputStreamReader(in)));
+        System.out.println("Script executed.");
+      } catch (SQLException ex) {
+        Logger.getLogger(InsertSQL.class.getName()).log(Level.SEVERE, null, ex);
+      }
     }
-
   }
-
 }
